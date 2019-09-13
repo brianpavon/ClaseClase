ae0f0c1cd83dbd66b7591687b64fb1ec50426649
alumno <alumno@LAB00PC00>
2019-09-12 22:09:23 -0300

    FIN CLASE 8,falta agregar empleados.c y empleados.h

---
+++ Clase08/src/utn.h
@@ -0,0 +1,24 @@
+/*
+ * utn.h
+ *
+ *  Created on: 12 sep. 2019
+ *      Author: alumno
+ */
+
+#ifndef UTN_H_
+#define UTN_H_
+#ifndef UTN_H_
+#define UTN_H_
+
+int getString(	char *pResultado,
+				char *pMensaje,
+				char *pMensajeError,
+				int minimo,
+				int maximo,
+				int reintentos);
+
+
+#endif /* UTN_H_ */
+
+
+#endif /* UTN_H_ */
